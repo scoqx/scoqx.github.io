c8f812cb0c1f98edeeb2d24c2ea0d37f2cc92f87
diwoc <diwoc@proton.me>
2025-06-12 15:39:09 +0300

    v3

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("docs/version.json")
+  fetch("version.json")
     .then(response => response.json())
     .then(data => {
       const version = data.version;
